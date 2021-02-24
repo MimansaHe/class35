6f3b36d48ef3002d6b8296d600b6474921a07f37
MimansaHe <76689108+MimansaHe@users.noreply.github.com>
2021-02-24 17:52:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var hypnoball;
+var database;
+var position;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    hypnoball = createSprite(250,250,10,10);
+    hypnoball.shapeColor = "red";
+    var hypnoballposition =database.ref("ball/position");
+    hypnoballposition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+
+   
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+
+function writePosition(x,y){
+  database.ref("ball/position").set({
+      'x':position.x+x,
+      "y":position.y+y
+  })
+}
+function readPosition(data){
+    position=data.val();
+    hypnoball.x=position.x;
+    hypnoball.y=position.y;
+}
+function showError(){
+    console.log("error in database");
+}
